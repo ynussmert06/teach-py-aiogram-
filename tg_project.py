c83d5c3ed5811262fa046f4c92df3b60a2d0a2f8
ynussmert06 <251485177+ynussmert06@users.noreply.github.com>
2026-01-31 22:29:53 +0300

    add inlane keyboard; show and redact choice;

--- tg_project.py
+++ tg_project.py
@@ -9,12 +9,14 @@ from aiogram import Bot, Dispatcher
 
 from tg_app import echobot
 from tg_app import wheather
+from tg_app import opros
 
 async def main():
     bot = Bot(token='')
     dp = Dispatcher()
     # dp.include_router(echobot.router_main)
-    dp.include_router(wheather.router_wheather)
+    # dp.include_router(wheather.router_wheather)
+    dp.include_router(opros.router_opros)
     await dp.start_polling(bot)
 if __name__ == '__main__':
     try:
